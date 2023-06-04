c26bd6ed7f14526c87fcd175392cccec65c459fb
TheAndrewNguyen <andrewbvietnguyen@gmail.com>
2023-06-03 23:18:15 -0700

    I'm working a lot with database on server.js please touch on the routes and export the routes

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express') 
 const app = express();   
+//database connection
+const db = require("./db.js")
+app.use(express.json()) // => req.body
+
 
 
 app.listen(3000) 
 app.set('view engine', 'ejs') 
 
+
+
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index")  
+    
 })  
 
+
+app.post("/database_query", (req, res) => {  
+    const { name } = req.body;
+    const { age } = req.body; 
+    const { gender } = req.body; 
+    const { disorder } = req.body; 
+
+
+    console.log("end")
+    res.send(name)  
+
+})
 
